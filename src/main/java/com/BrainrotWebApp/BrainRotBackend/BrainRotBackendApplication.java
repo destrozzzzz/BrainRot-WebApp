99b84486149b367ed0023ac629b50956c1946a57
Syed Irfan Ahmed <haroonirfan023@gmail.com>
2025-08-07 14:35:49 +0530

    Added the project to git

---
+++ src/main/java/com/BrainrotWebApp/BrainRotBackend/BrainRotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.BrainrotWebApp.BrainRotBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainRotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainRotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BrainrotWebApp/BrainRotBackend/BrainRotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BrainrotWebApp.BrainRotBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainRotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
